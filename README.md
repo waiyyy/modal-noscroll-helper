59bbde372171b40bcc507422aa2b4fca712d3820
waiyyy <yang_qqy@outlook.com>
2019-09-25 18:46:46 +0800

    测试

--- README.md
+++ README.md
@@ -10,5 +10,23 @@
 2、import ModalNoScrollHelper from 'modal-noscroll-helper'
 
 3、let modalNoScrollHelper = new ModalNoScrollHelper();
+
    弹窗打开： modalNoScrollHelper.toggleBlock(true)
+
    弹窗关闭： modalNoScrollHelper.toggleBlock(false)
+
+4、 页面加上这两个样式
+
+body.modal-open{
+  position: fixed;
+  width: 100%;
+}
+
+body.modal-open-input{
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
--- static/index.js
+++ static/index.js
@@ -1,59 +1,62 @@
-import style from './index.scss';
-
-class ModalNoScrollHelper {
-    
-    static defaultOptions = {
+function ModalNoScrollHelper(options) {
+    var defaultOptions = {
         modalClass: 'modal-open',
         withInput: false
     };
 
-    defaults(options) {
-        return {...ModalNoScrollHelper.defaultOptions, ...options};
-    }
-
-    constructor(options) {
-        this.$body = document.querySelector("body");
-        this.scrollTop = 0;
-        this.options = this.defaults(options);
-    }
-
-    blockScroll() {
-        let self = this;
-        let $body = this.$body;
-        self.scrollTop = this.getScrollTop();
-        $body.classList.add(this.options.modalClass);
-        if(!this.options.withInput){
-            $body.style.top= -self.scrollTop;
-        
+    var merge = function(defaultOptions, options) {
+        var newObj = {};
+        for (var i in defaultOptions) {
+            newObj[i] = defaultOptions[i];
         }
-    }
-
-    unBlockScroll() {
-        let self = this;
-        let $body = self.$body;
-        $body.classList.remove(self.options.modalClass);
-        this.setScrollTop(self.scrollTop);
-        $body.style.top= 0;      
-    }
-
-    getScrollTop() {
-        if (document.documentElement.scrollTop > 0) {
-            this.scrollTopElem = document.documentElement;
-            return document.documentElement.scrollTop;
-        } else {
-            this.scrollTopElem = document.body;
-            return document.body.scrollTop;
+        for (var i in options) {
+            newObj[i] = options[i];
         }
+        return newObj;
     }
+    this.options  = merge(defaultOptions, options);
+    this.$body = document.querySelector("body");
+    this.scrollTop = 0;
+}
 
-    setScrollTop(top) {
-        return this.scrollTopElem.scrollTop = top;
-    }
-
-    toggleBlock(isBlock) {
-        isBlock ? this.blockScroll() : this.unBlockScroll();
+ModalNoScrollHelper.prototype.blockScroll = function() {
+    var self = this;
+    var $body = this.$body;
+    self.scrollTop = this.getScrollTop();
+    $body.classList.add(this.options.modalClass);
+    if(!this.options.withInput){
+        $body.style.top= -self.scrollTop;
+    
     }
 }
 
+ModalNoScrollHelper.prototype.unBlockScroll = function() {
+    var self = this;
+    var $body = self.$body;
+    $body.classList.remove(self.options.modalClass);
+    this.setScrollTop(self.scrollTop);
+    $body.style.top= 0;      
+}
 
-export default ModalNoScrollHelper;
+ModalNoScrollHelper.prototype.getScrollTop = function() {
+    if (document.documentElement.scrollTop > 0) {
+        this.scrollTopElem = document.documentElement;
+        return document.documentElement.scrollTop;
+    } else {
+        this.scrollTopElem = document.body;
+        return document.body.scrollTop;
+    }
+}
+
+ModalNoScrollHelper.prototype.setScrollTop = function(top) {
+    return this.scrollTopElem.scrollTop = top;
+}
+
+ModalNoScrollHelper.prototype.toggleBlock = function(isBlock) {
+    isBlock ? this.blockScroll() : this.unBlockScroll();
+}
+
+module.exports = {
+    ModalNoScrollHelper
+}
+
